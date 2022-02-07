b151f48772042d1c366b91be026def04aaa66425
ijadams <ijadams503@gmail.com>
2022-02-07 11:03:22 -0600

    ban d

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,31 +8,94 @@
           </div>
         </div>
       </div>
-      <div class="behind" style="left: 25px;">
-        <img src="https://www.cameronsworld.net/img/sprites/sprite18/table/left.png"/>
-      </div>
-      <div class="behind" style="right: 25px;">
-        <img src="https://www.cameronsworld.net/img/content/20/25.gif"/>
-      </div>
-      <div class="behind" style="right: 25px; bottom: 0; top: auto;">>
-        <img src="https://www.cameronsworld.net/img/content/20/14.gif">
-      </div>
-      <div class="behind" style="right: 150px; bottom: 150px; top: auto;">>
-        <img src="https://www.cameronsworld.net/img/content/20/8.gif">
-      </div>
 
-      <div class="behind" style="left: 150px; bottom: 150px; top: auto;">>
-        <img src="https://www.cameronsworld.net/img/content/7/8.gif">
-      </div>
+      <section class="retro">
 
-      <div class="behind" style="left: 120px; bottom: 200px; top: auto;">>
-        <img src="https://www.cameronsworld.net/img/content/7/9.gif">
-      </div>
+        <div class="behind" style="left: 25px;">
+          <img src="https://www.cameronsworld.net/img/sprites/sprite18/table/left.png"/>
+        </div>
 
-      <div class="behind" style="left: 180px; bottom: 126px; top: auto;">>
-        <img src="https://www.cameronsworld.net/img/content/7/10.gif">
-      </div>
 
+        <div class="behind" style="right: 25px; top:80px;">
+          <img src="https://www.cameronsworld.net/img/content/20/10.gif"/>
+        </div>
+
+        <div class="behind" style="right: 25px;">
+          <img src="https://www.cameronsworld.net/img/content/20/25.gif"/>
+        </div>
+
+        <div class="behind" style="right: 25px; bottom: 0; top: auto;">>
+          <img src="https://www.cameronsworld.net/img/content/20/14.gif">
+        </div>
+
+        <div class="behind" style="right: 150px; bottom: 150px; top: auto;">>
+          <img src="https://www.cameronsworld.net/img/content/20/8.gif">
+        </div>
+
+        <div class="behind" style="right: 150px; bottom: 350px; top: auto;">>
+          <img src="https://www.cameronsworld.net/img/content/33/18.gif">
+        </div>
+
+
+        <div class="behind" style="left: 150px; bottom: 150px; top: auto;">>
+          <img src="https://www.cameronsworld.net/img/content/7/8.gif">
+        </div>
+
+        <div class="behind" style="left: 120px; bottom: 200px; top: auto;">>
+          <img src="https://www.cameronsworld.net/img/content/7/9.gif">
+        </div>
+
+        <div class="behind" style="left: 180px; bottom: 126px; top: auto;">>
+          <img src="https://www.cameronsworld.net/img/content/7/10.gif">
+        </div>
+
+        <div class="behind" style="left: 250px; bottom: 30px; top: auto;">>
+          <img src="https://www.cameronsworld.net/img/content/18/11.gif">
+        </div>
+
+        <div class="behind" style="right: 250px; bottom: 500px; top: auto;">
+          <img src="https://www.cameronsworld.net/img/content/20/7.gif">
+        </div>
+
+        <div class="behind">
+          <img src="https://www.cameronsworld.net/img/sprites/sprite20.png"/>
+        </div>
+
+        <div class="behind" style="right: 150px;">
+          <img src="https://res.cloudinary.com/ia-interactive/image/upload/v1644252818/favicon_uz3xb8.png">
+        </div>
+
+
+        <div class="behind" style="right: 180px;">
+          <img src="https://res.cloudinary.com/ia-interactive/image/upload/v1644253162/drum1-c_kbvl2r.gif">
+        </div>
+
+        <div class="behind" style="right: 380px;">
+          <img src="https://res.cloudinary.com/ia-interactive/image/upload/v1644253162/motorcycle-b_oetsvc.gif">
+        </div>
+
+        <div class="behind" style="bottom: 0; right: 380px; top: auto;">
+          <img src="https://res.cloudinary.com/ia-interactive/image/upload/v1644253162/cdrom1-a_mg1j3s.gif">
+        </div>
+
+        <div class="behind" style="bottom: 120px; right: 380px; top: auto;">
+          <img src="https://res.cloudinary.com/ia-interactive/image/upload/v1644253162/mower-b_uapjmz.gif">
+        </div>
+
+        <div class="behind" style="bottom: 150px; right: 280px; top: auto;">
+          <img src="https://res.cloudinary.com/ia-interactive/image/upload/v1644253162/car02-b_oo8nzt.gif">
+        </div>
+
+        <div class="behind" style="bottom: 180px; right: 15px; top: auto;">
+          <img src="https://res.cloudinary.com/ia-interactive/image/upload/v1644253163/football-b_dhgq6z.gif">
+        </div>
+
+
+        <div class="behind" style="bottom: 100px; right: 35px; top: auto;">
+          <img src="https://res.cloudinary.com/ia-interactive/image/upload/v1644253163/ball1-a_tjxs2o.gif">
+        </div>
+
+      </section>
     </section>
   </Layout>
 </template>
